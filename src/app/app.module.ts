6b8a927c5c72307959a3887b470cc6266ad39482
tihomir22 <tihomir_alcudia3@hotmail.com>
2018-08-20 09:57:08 +0300

    Creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './componentes/homepage/homepage.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { RegisterpageComponent } from './componentes/registerpage/registerpage.component';
+import { LoginpageComponent } from './componentes/loginpage/loginpage.component';
+import { PrivadoComponent } from './componentes/privado/privado.component';
+import { NotfoundpageComponent } from './componentes/notfoundpage/notfoundpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NavbarComponent,
+    RegisterpageComponent,
+    LoginpageComponent,
+    PrivadoComponent,
+    NotfoundpageComponent
   ],
   imports: [
     BrowserModule,
