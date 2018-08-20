d9eaca2d368a170ead47bd14fab9c4c6c0ce0aa4
tihomir22 <tihomir_alcudia3@hotmail.com>
2018-08-20 10:26:18 +0300

    Modificado navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomepageComponent} from './componentes/homepage/homepage.component';
+import {LoginpageComponent} from './componentes/loginpage/loginpage.component';
+import {RegisterpageComponent} from './componentes/registerpage/registerpage.component';
+import {PrivadoComponent} from './componentes/privado/privado.component'; 
+import {NotfoundpageComponent} from './componentes/notfoundpage/notfoundpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomepageComponent},{path:'login',component:LoginpageComponent},{path:'register',component:RegisterpageComponent},{path:'privado',component:PrivadoComponent},{path:'**',component:NotfoundpageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+
+
+}
