50ff1725286c86c9e2b7af72187ef644d5c5e583
tihomir22 <tihomir_alcudia3@hotmail.com>
2018-08-20 15:07:20 +0300

    Dependencias firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyAzmbNbApm0kSDAphLrU3qrtEtBqgjyBlE",
+    authDomain: "loginregistro-5f183.firebaseapp.com",
+    databaseURL: "https://loginregistro-5f183.firebaseio.com",
+    projectId: "loginregistro-5f183",
+    storageBucket: "",
+    messagingSenderId: "1035975773140"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyAzmbNbApm0kSDAphLrU3qrtEtBqgjyBlE",
+    authDomain: "loginregistro-5f183.firebaseapp.com",
+    databaseURL: "https://loginregistro-5f183.firebaseio.com",
+    projectId: "loginregistro-5f183",
+    storageBucket: "",
+    messagingSenderId: "1035975773140"
+  }
 };
 
 /*
